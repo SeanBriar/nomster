49b567d4dd6ce55634b7c6a70c41d515161638b9
Sean Briar O'Neill <sean.briar1011@gmail.com>
2019-06-24 00:01:10 +0000

    added pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create(name: 'That One Place', address: '3425 Las Vegas Blv, Las Vegas, NV', description: 'Where all those memories get created')
+
+Place.create(name: 'Bobs Burgers', address: '425 Anywhere St., Anytown, USA', description: 'Delicious burgers')
+
+Place.create(name: 'Monks Restaurant', address: '29683 Main St., New York City, NY', description: 'Big Salad for All')
+
+Place.create(name: 'Clarks Restaurant', address: '29683 Main St., New York City, NY', description: 'Big Salad for All')
+
+Place.create(name: 'Peabodys', address: '11 Front, Ashland, NY', description: 'Everyones favrotie')
+
+Place.create(name: 'BBQ Madness', address: '29683 Main St., Nashville, Alabama', description: 'Lets get greasy')

6f7472dcbc0a4460a43508896caaa8506cd9fd08
Sean Briar O'Neill <sean.briar1011@gmail.com>
2019-06-26 17:44:26 +0000

    complete user destroy

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,26 @@
 class PlacesController < ApplicationController
-   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.paginate(page: params[:page], per_page: 4)
   end
 
+
   def new
     @place = Place.new
   end
+
 
   def create
       current_user.places.create(place_params)
       redirect_to root_path
   end
 
+
   def show
     @place = Place.find(params[:id])
   end
+
 
   def edit
     @place = Place.find(params[:id])
@@ -26,17 +30,26 @@ class PlacesController < ApplicationController
      end
   end
 
+
   def update
     @place = Place.find(params[:id])
+
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
 
+
   def destroy
     @place = Place.find(params[:id])
+    
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
     @place.destroy
     redirect_to root_path
   end
